13a037d68d60b67b9a0eb7774b048db128eaaea2
ilham ardiansa <ilhamardiansya8890@gmail.com>
2024-07-02 08:46:12 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,7 @@ async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new CustomValidationPipe({
-  //   whitelist: true,
-  //   forbidNonWhitelisted: true,
-  //   transform: true,
-  // }));
-  // app.useGlobalFilters(new CustomExceptionFilter());
+  app.useGlobalFilters(new CustomExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
