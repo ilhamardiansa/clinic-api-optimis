b3162494d6870b22c7e9c665bc067719724f66f0
ilham ardiansa <ilhamardiansya8890@gmail.com>
2024-07-06 20:06:30 +0700

    update

--- src/middleware/role.guard.ts
+++ src/middleware/role.guard.ts
@@ -1,12 +1,12 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, HttpException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/entity/profile/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Role } from 'src/entity/role.entity';
 import { format_json } from 'src/env';
+import { Response } from 'express';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -28,9 +28,11 @@ export class RolesGuard implements CanActivate {
     }
     
     const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
     const token = request.headers.authorization?.split(' ')[1]; 
     if (!token) {
-      throw new UnauthorizedException('Token not found');
+      this.throwFormattedException(response, 400, 'Token not found', 'UnauthorizedException');
+      return false;
     }
 
     try {
@@ -38,6 +40,7 @@ export class RolesGuard implements CanActivate {
       const user = await this.findUserById(decodedToken.userId);
 
       if (!user || !user.role_id) {
+        this.throwFormattedException(response, 403, 'Access denied', 'ForbiddenException');
         return false;
       }
 
@@ -45,17 +48,34 @@ export class RolesGuard implements CanActivate {
       const userRoleIds = userRoles.map(role => role.id);
 
       const hasRole = userRoleIds.includes(user.role_id);
+      if (!hasRole) {
+        this.throwFormattedException(response, 403, 'Access denied', 'ForbiddenException');
+        return false;
+      }
       return hasRole;
     } catch (error) {
-      throw new UnauthorizedException('Invalid token');
+      this.throwFormattedException(response, 400, 'Invalid token', 'UnauthorizedException');
+      return false;
     }
   }
 
   async findUserById(userId: number): Promise<User> {
-    return this.userRepository.findOne({ where : { id: userId } });
+    return this.userRepository.findOne({ where: { id: userId } });
   }
 
   async findRole(role: string): Promise<Role> {
-    return this.roleRepository.findOne({ where : { name: role } });
+    return this.roleRepository.findOne({ where: { name: role } });
+  }
+
+  private throwFormattedException(response: Response, status: number, message: string, exceptionType: string) {
+    const formattedResponse = format_json(
+      status,
+      false,
+      message,
+      null,
+      message,
+      null,
+    );
+    response.status(status).json(formattedResponse);
   }
 }
