ea610b4cbf43c2fcb6638cf40d9014775dc20cac
ilham ardiansa <ilhamardiansya8890@gmail.com>
2024-07-10 14:58:12 +0700

    update swagger

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .addTag('api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
